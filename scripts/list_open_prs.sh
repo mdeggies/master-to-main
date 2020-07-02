786562c9ea92f1df822ee0a9559313e3bd6209a3
Michele <mdeggies@gmail.com>
2020-07-01 23:30:38 -0700

    test

--- scripts/list_open_prs.sh
+++ scripts/list_open_prs.sh
@@ -1,1 +1,5 @@
-echo $OPEN_PRS
+set -e
+
+curl -H "Authorization: token $1" -H "Accept: 'application/vnd.github.v3+json'" "https://api.github.com/repos/$2/$3/pulls?state=open" -O hello.txt
+
+echo hello.txt
